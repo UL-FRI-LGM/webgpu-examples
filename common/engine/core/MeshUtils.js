f23fe60817299ee292cbb20ec8e9d5cc233bba72
Žiga Lesar <terier.kuza@gmail.com>
2023-09-21 17:33:59 +0200

    Add AABB-AABB example

--- common/engine/core/MeshUtils.js
+++ common/engine/core/MeshUtils.js
@@ -17,3 +17,27 @@ export function transformMesh(mesh, matrix,
         transformVertex(vertex, matrix, normalMatrix, tangentMatrix);
     }
 }
+
+export function calculateAxisAlignedBoundingBox(mesh) {
+    const initial = {
+        min: vec3.clone(mesh.vertices[0].position),
+        max: vec3.clone(mesh.vertices[0].position),
+    };
+
+    return {
+        min: mesh.vertices.reduce((a, b) => vec3.min(a, a, b.position), initial.min),
+        max: mesh.vertices.reduce((a, b) => vec3.max(a, a, b.position), initial.max),
+    };
+}
+
+export function mergeAxisAlignedBoundingBoxes(boxes) {
+    const initial = {
+        min: vec3.clone(boxes[0].min),
+        max: vec3.clone(boxes[0].max),
+    };
+
+    return {
+        min: boxes.reduce(({ min: amin }, { min: bmin }) => vec3.min(amin, amin, bmin), initial),
+        max: boxes.reduce(({ max: amax }, { max: bmax }) => vec3.max(amax, amax, bmax), initial),
+    };
+}
