78e017540322c8b69192d0a07082d8d6dac7c3c6
Žiga Lesar <terier.kuza@gmail.com>
2023-09-22 00:04:00 +0200

    Add animation example

---
+++ examples/06-animation/01-easing-functions/main.js
@@ -0,0 +1,56 @@
+import { quat, vec3, mat4 } from '../../../lib/gl-matrix-module.js';
+
+import { ResizeSystem } from '../../../common/engine/systems/ResizeSystem.js';
+import { UpdateSystem } from '../../../common/engine/systems/UpdateSystem.js';
+
+import { GLTFLoader } from '../../../common/engine/loaders/GLTFLoader.js';
+import { UnlitRenderer } from '../../../common/engine/renderers/UnlitRenderer.js';
+import { OrbitController } from '../../../common/engine/controllers/OrbitController.js';
+import { getGlobalModelMatrix } from '../../../common/engine/core/SceneUtils.js';
+import { Camera, Transform } from '../../../common/engine/core.js';
+
+import * as EasingFunctions from '../../../common/engine/animators/EasingFunctions.js';
+
+const canvas = document.querySelector('canvas');
+const renderer = new UnlitRenderer(canvas);
+await renderer.initialize();
+
+const loader = new GLTFLoader();
+await loader.load('scene/scene.gltf');
+
+const scene = loader.loadScene(loader.defaultScene);
+const camera = loader.loadNode('Camera');
+camera.addComponent(new OrbitController(camera, canvas, {
+    distance: 10,
+    pitch: -0.1,
+}));
+
+const cube = loader.loadNode('Cube');
+const start = loader.loadNode('Start');
+const end = loader.loadNode('End');
+
+const startPosition = mat4.getTranslation(vec3.create(), getGlobalModelMatrix(start));
+const endPosition = mat4.getTranslation(vec3.create(), getGlobalModelMatrix(end));
+
+function update(t, dt) {
+    const time = t % 1;
+    const transform = cube.getComponentOfType(Transform);
+    vec3.lerp(transform.translation, startPosition, endPosition, EasingFunctions.bounceEaseOut(time));
+
+    scene.traverse(node => {
+        for (const component of node.components) {
+            component.update?.(t, dt);
+        }
+    })
+}
+
+function render() {
+    renderer.render(scene, camera);
+}
+
+function resize({ displaySize: { width, height }}) {
+    camera.getComponentOfType(Camera).aspect = width / height;
+}
+
+new ResizeSystem({ canvas, resize }).start();
+new UpdateSystem({ update, render }).start();
