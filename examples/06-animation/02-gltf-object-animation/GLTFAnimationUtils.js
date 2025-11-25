14281af20838f711704b8c38496fee10f417181b
Gorazd Gorup <gorupgorazd@gmail.com>
2025-11-25 15:45:38 +0100

    GLTF animation restructuring

--- examples/06-animation/02-gltf-object-animation/GLTFAnimationUtils.js
+++ examples/06-animation/02-gltf-object-animation/GLTFAnimationUtils.js
@@ -1,20 +1,10 @@
-function parseAnimationSampler(gltfSpec) {
+import { Animation } from './Animation.js';
+import { Transform } from 'engine/core/core.js';
 
-}
-
-function parseAnimationChannel(gltfSpec, scene) {
+function loadAnimationSampler(loader, sampler) {
     const keyframes = [];
     const values = [];
 
-    const target = channel.target;
-    if (target.node === undefined) {
-        return;
-    }
-    
-    const entity = scene[target.node];
-    const transform = entity.getComponentOfType(Transform);
-    
-    const sampler = gltfSpec.samplers[channel.sampler];
     const keyframeAccessor = loader.loadAccessor(sampler.input);
     const interpolation = sampler.interpolation ?? 'LINEAR';
     const valuesAccessor = loader.loadAccessor(sampler.output);
@@ -23,19 +13,31 @@ function parseAnimationChannel(gltfSpec, scene) {
         keyframes.push(...keyframeAccessor.get(i));
         values.push(valuesAccessor.get(i));
     }
+    return { keyframes, values, interpolation };
+}
+
+function loadAnimationChannel(loader, channel, samplers, scene) {
+    const target = channel.target;
+    if (target.node === undefined) {
+        return;
+    }
+
+    const entity = scene[target.node];
+    const transform = entity.getComponentOfType(Transform);
+    const sampler = samplers[channel.sampler];
 
     const animation = new Animation({
         transform,
         type: target.path,
-        interpolation,
-        keyframes,
-        values,
+        interpolation: sampler.interpolation,
+        keyframes: sampler.keyframes,
+        values: sampler.values,
         fps: 1
     });
     entity.addComponent(animation);
 }
 
-export function parseAnimation(gltfSpec, scene) {
+function loadAnimation(loader, gltfSpec, scene) {
     if (loader.cache.has(gltfSpec)) {
         return loader.cache.get(gltfSpec);
     }
@@ -44,7 +46,15 @@ export function parseAnimation(gltfSpec, scene) {
         return null;
     }
 
+    const samplers = gltfSpec.samplers.map(s => loadAnimationSampler(loader, s));
+
     for (const channel of gltfSpec.channels) {
-        parseAnimationChannel(channel, scene);
+        loadAnimationChannel(loader, channel, samplers, scene);
+    }
+}
+
+export function loadAnimations(loader, scene) {
+    for (const animation of loader.gltf.animations) {
+        loadAnimation(loader, animation, scene);
     }
 }
